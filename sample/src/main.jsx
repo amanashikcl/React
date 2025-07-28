1b23a6fb43b5bf705be9a51d503150310fdee646
amanashikcl <amanashikcloud@outlook.com>
2025-07-28 14:20:13 +0530

    testing

--- sample/src/main.jsx
+++ sample/src/main.jsx
@@ -1,10 +1,10 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
-
+import App from "./App"
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    
+    <p id="array">Array:</p>
+    <button onClick={App}>Click here for array</button>
   </StrictMode>,
 )

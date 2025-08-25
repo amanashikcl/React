b14c12d72602be7d1f86216a8ec166abbd989f39
amanashikcl <amanashikcloud@outlook.com>
2025-08-25 12:37:35 +0530

    useEffect

---
+++ useEffect/src/App.jsx
@@ -0,0 +1,39 @@
+import { useEffect, useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App(){
+  const [count, setCount] = useState(0)
+  const [color, setColor] = useState("white")
+
+  useEffect(()=> {
+    console.log("Inside useEffect", count);
+
+    return () =>{
+      console.log("clean up", count);
+    }
+  },[])
+
+  return(
+    <div style={{backgroundColor:color, height:"100vh"}}>
+      <p style={{color:"red"}}>{count}</p>
+      <button
+      onClick={()=>{
+        setCount(count+1)
+      }}
+      >
+        Click me
+      </button>
+      <button
+      onClick={()=>{
+        setColor(color==="white" ? "green" : "white")
+      }}
+      >
+        Toggle Color
+      </button>
+    </div>
+  )
+}
+
+export default App
